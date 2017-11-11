d9e46b89fe199e5edd381ede8be45e5d3cb25161
dzsoniriko <dzsoniriko@gmail.com>
2017-11-11 23:52:06 +0100

    basic face and eye detection on image and on cam

--- basic_cam_face_eye_detector.py
+++ basic_cam_face_eye_detector.py
@@ -7,12 +7,17 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 # Create the haar cascade
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+faceCascade = cv2.CascadeClassifier("haarclassifiers/haarcascade_frontalface_default.xml")
+eyeCascade = cv2.CascadeClassifier("haarclassifiers/haarcascade_eye_tree_eyeglasses.xml")
 
 while(True):
 	# Capture frame-by-frame
 	ret, frame = cap.read()
 
+	if not ret:
+		print("No camera found...")
+		break
+		
 	# Our operations on the frame come here
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -25,15 +30,28 @@ while(True):
 		#flags = cv2.CV_HAAR_SCALE_IMAGE
 	)
 
-	print("Found {0} faces!".format(len(faces)))
+	eyes = eyeCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=20,
+		minSize=(10, 10)
+		#flags = cv2.CV_HAAR_SCALE_IMAGE
+	)
+
+	print("Found {0} faces! Found {1} eyes".format(len(faces), len(eyes)))
 
 	# Draw a rectangle around the faces
 	for (x, y, w, h) in faces:
-		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)		
 
+	# Draw a circle around the eye
+	for (x, y, w, h) in eyes:
+		cv2.circle(frame, ( int( x + w / 2), int( y + w / 2) ), int( w / 2 ), (0, 0, 255), 2)
 
 	# Display the resulting frame
+	#cv2.imshow('frame', frame)	
 	cv2.imshow('frame', frame)
+
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
--- basic_image_face_detector.py
+++ basic_image_face_detector.py
@@ -2,8 +2,11 @@ import cv2
 import sys
 
 # Get user supplied values
-imagePath = sys.argv[1]
-cascPath = "haarcascade_frontalface_default.xml"
+if len( sys.argv ) < 2:
+    imagePath = "images/abba.png"
+else:
+    imagePath = sys.argv[1]
+cascPath = "haarclassifiers/haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
